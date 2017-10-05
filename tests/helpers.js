59c504326dc93130df08edbd1d810e1e3f114a26
iliran11 <yh6mphx5>
2017-10-05 12:25:00 +0300

    3 tests completed

---
+++ tests/helpers.js
@@ -0,0 +1,6 @@
+export function getRandomIntInclusive(min, max) {
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
--- tests/list.test.js
+++ tests/list.test.js
@@ -1,12 +1,24 @@
 const { List } = require('../itsy-bitsy-data-structures.js')
 
-describe('List Creation', () => {
+describe('Creation and pushing 3 numbers', () => {
+    var list = new List()
+    list.push(4);
+    list.push(1);
+    list.push(6);
     test('New list is Created', () => {
-        var list = new List()
         expect(list).toBeDefined();
     });
-    test('New list memory is empty', () => {
-        memoryLength = new List().memory.length
-        expect(memoryLength).toBe(0);
+    test('New list memory is equal to 1', () => {
+        const memoryLength = list.memory.length
+        expect(memoryLength).toBe(3);
     });
+    test('first address is correct', () => {
+        firstMemoryAddressValue = list.get(0)
+        expect(firstMemoryAddressValue).toBe(4)
+    })
+    test('last address is correct', () => {
+        firstMemoryAddressValue = list.get(2)
+        expect(firstMemoryAddressValue).toBe(6)
+    })
 })
+// describe('shifting to a list with 3 number elements')
